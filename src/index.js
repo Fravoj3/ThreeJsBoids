4e27d973d6745630f1b5dfd11ff1db4a2b766a7e
Fravoj Envy <fravoj3@gmail.com>
2024-05-05 11:44:58 +0200

    boids prepared for simulation

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import "./style.scss";
 import model from "./models/bird.gltf";
 
-let scene, camera, renderer, controls, gridHelper, mixer, clock;
+let scene, camera, renderer, controls, gridHelper, boid, clock;
+const mixer = []
+const boidContainerSize = {x: 100, y: 50, z: 100};
+const boidCount = 100;
+const boids = [];
 
 init();
 animate();
@@ -29,21 +33,17 @@ controls = new OrbitControls( camera, renderer.domElement );
 camera.position.set( 0, 20, 100 );
 controls.update();
 
-gridHelper = new THREE.GridHelper( 10, 10 );
+gridHelper = new THREE.GridHelper( boidContainerSize.x, 10 );
 scene.add( gridHelper );
 
 const loader = new GLTFLoader();
-loader.load( model, function ( gltf ) {
-    console.log(gltf.scene);
-    gltf.scene.scale.set(20, 20, 20);
+loader.load( model, ( gltf )=>{
+    gltf.scene.scale.set(50, 50, 50);
     gltf.scene.children[0].material = new THREE.MeshStandardMaterial({color: 0x00ff00, side: THREE.DoubleSide});
-    scene.add( gltf.scene );
+    
+    boid = gltf;
+    addBoids();
 
-    mixer = new THREE.AnimationMixer( gltf.scene );
-    gltf.animations.forEach( ( clip ) => {   
-            mixer.clipAction( clip ).play();
-          
-        } );
 }, undefined, function ( error ) {
     console.error( error );
 } );
@@ -52,15 +52,40 @@ loader.load( model, function ( gltf ) {
 }
 
 function animate() {
-
 	requestAnimationFrame( animate );
 
 	// required if controls.enableDamping or controls.autoRotate are set to true
 	controls.update();  
     const delta = clock.getDelta();
   
-    if ( mixer ) mixer.update( delta );
+    boids.forEach(boid => {
+        boid.mixer.update(delta);
+    });
 
 	renderer.render( scene, camera );
 
+}
+function addBoids(){
+    const {x, y, z} = boidContainerSize;
+    const geometry = new THREE.BoxGeometry(x, y, z)
+    geometry.translate(0, 25, 0);
+    const wireframe = new THREE.WireframeGeometry( geometry );
+
+    const line = new THREE.LineSegments( wireframe );
+    line.material.depthTest = false;
+    line.material.opacity = 0.25;
+    line.material.color.setHex( 0x144696 );
+    line.material.transparent = true;
+    scene.add( line );
+
+    for(let i = 0; i < boidCount; i++){
+        const clone = boid.scene.clone();
+        clone.position.set(Math.random() * x - x/2, Math.random() * y, Math.random() * z - z/2);
+        scene.add(clone);
+        const mixer = new THREE.AnimationMixer( clone );
+        boid.animations.forEach( ( clip ) => {   
+            mixer.clipAction( clip ).play();
+        } );
+        boids.push({object: clone, mixer: mixer});
+    }
 }
